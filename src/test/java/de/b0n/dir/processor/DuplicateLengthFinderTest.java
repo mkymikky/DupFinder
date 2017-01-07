04f053d5c662ff718e5d892eed0916893dc5a0dd
FunThomas424242 <funthomas424242@gmail.com>
2017-01-07 15:28:32 +0100

    unit test for class DuplicateLengthFinder added
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

---
+++ src/test/java/de/b0n/dir/processor/DuplicateLengthFinderTest.java
@@ -0,0 +1,69 @@
+package de.b0n.dir.processor;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.Queue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+/**
+ * Created by huluvu424242 on 07.01.17.
+ */
+public class DuplicateLengthFinderTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test
+    public void createValidInstanceWithValidParameters() {
+        final ExecutorService threadPool = Executors.newWorkStealingPool();
+        final File folder = new File("src/test/resources/");
+        final Queue<Queue<File>> result = DuplicateLengthFinder.getResult(threadPool, folder);
+        assertNotNull(result);
+        assertEquals("falsche Anzahl an Duplikaten bestimmt",1,result.size());
+        assertEquals("falsche Anzahl von Test1.txt Vorkommen bestimmt",4,result.element().size());
+    }
+
+    @Test
+    public void scanForDuplicateGivenEmptyFolder() {
+        final ExecutorService threadPool = Executors.newWorkStealingPool();
+        final File folder = new File("src/test/resources/emptyFolder");
+        final Queue<Queue<File>> result = DuplicateLengthFinder.getResult(threadPool, folder);
+        assertNotNull(result);
+        assertEquals(0,result.size());
+    }
+
+    @Test
+    public void scanForDuplicateGivenDuplicate0() {
+        final ExecutorService threadPool = Executors.newWorkStealingPool();
+        final File folder = new File("src/test/resources/duplicate0");
+        final Queue<Queue<File>> result = DuplicateLengthFinder.getResult(threadPool, folder);
+        assertNotNull(result);
+        assertEquals(0,result.size());
+    }
+
+    @Test
+    public void scanForDuplicateGivenDuplicate1() {
+        final ExecutorService threadPool = Executors.newWorkStealingPool();
+        final File folder = new File("src/test/resources/duplicate1");
+        final Queue<Queue<File>> result = DuplicateLengthFinder.getResult(threadPool, folder);
+        assertNotNull(result);
+        assertEquals(1,result.size());
+        assertEquals(2,result.element().size());
+    }
+
+}
