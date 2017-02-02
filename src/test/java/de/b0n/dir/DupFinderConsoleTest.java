7a5aeeefdc635bbe480ae91b87bf9b08d9d11125
Claus Wollnik <c.wollnik@web.de>
2017-02-03 00:15:51 +0100

    Revert "Merge branch 'master' of https://github.com/mkymikky/DupFinder"
    This reverts commit eebc8fa81ea850870496a7530dd5a21dcc61b074, reversing
    changes made to a8e28ffbcc16da2be8a44a92479825ced43b1ed1.

---
+++ src/test/java/de/b0n/dir/DupFinderConsoleTest.java
@@ -0,0 +1,87 @@
+package de.b0n.dir;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assume.assumeTrue;
+
+public class DupFinderConsoleTest {
+	private static final String PATH_FILE = "src/test/resources/Test1.txt";
+	private static final String PATH_EMPTY_FOLDER = "src/test/resources/emptyFolder";
+    private static final String PATH_SAME_SIZE_FILES_IN_TREE_FOLDER = "src/test/resources/duplicateTree";
+
+	private PrintStream printStream;
+	private ByteArrayOutputStream byteArrayOutputStream;
+
+	@Before
+	public void setUp() throws IOException {
+		byteArrayOutputStream = new ByteArrayOutputStream();
+		printStream = new PrintStream(byteArrayOutputStream);
+	}
+
+	@After
+	public void tearDown() throws IOException {
+	}
+
+	@Test
+	public void testNoArgument() {
+		System.setErr(printStream);
+		DupFinderConsole.main(new String[] {});
+		assertEquals(
+				"FEHLER: Parameter <Verzeichnis> fehlt.\r\n Benutzung: DupFinder <Verzeichnis>\r\n<Verzeichnis> = Verzeichnis in dem rekursiv nach Duplikaten gesucht wird",
+				new String(byteArrayOutputStream.toByteArray(), StandardCharsets.UTF_8).trim());
+	}
+
+	@Test
+	public void testArgumentIsFile() {
+		System.setErr(printStream);
+		DupFinderConsole.main(new String[] {PATH_FILE});
+		assertEquals(
+				"FEHLER: Parameter <Verzeichnis> ist kein Verzeichnis.\r\n Benutzung: DupFinder <Verzeichnis>\r\n<Verzeichnis> = Verzeichnis in dem rekursiv nach Duplikaten gesucht wird",
+				new String(byteArrayOutputStream.toByteArray(), StandardCharsets.UTF_8).trim());
+	}
+
+	@Test
+	public void testPathIsEmpty() {
+		System.setErr(printStream);
+        final File folder = new File(PATH_EMPTY_FOLDER);
+        assumeTrue(folder.mkdir());
+    	DupFinderConsole.main(new String[] {PATH_EMPTY_FOLDER});
+    	assertTrue(new String(byteArrayOutputStream.toByteArray(), StandardCharsets.UTF_8).isEmpty());
+    	folder.delete();
+	}
+
+	@Test
+	public void testDuplicates() {
+		System.setOut(printStream);
+       	DupFinderConsole.main(new String[] {PATH_SAME_SIZE_FILES_IN_TREE_FOLDER});
+       	String output = new String(byteArrayOutputStream.toByteArray(), StandardCharsets.UTF_8);
+       	List<String> lines = Arrays.asList(output.split("\\r\\n|\\n|\\r"));
+       	listContainsLineEndingWith(lines, "Test1.txt");
+       	listContainsLineEndingWith(lines, "Test2.txt");
+	}
+
+	@Test
+	public void testConstructor() {
+		assertNotNull(new DupFinderConsole());
+	}
+
+	private void listContainsLineEndingWith(List<String> lines, String ending) {
+		for (String line : lines) {
+			if (line.endsWith(ending)) {
+				return;
+			}
+		}
+		fail("No line ends with " + ending);
+	}
+}
