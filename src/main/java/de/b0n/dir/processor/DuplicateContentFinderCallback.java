7a394b2346c4d6e03dccbc37712785eeb4c6e8ed
Claus Wollnik <c.wollnik@web.de>
2018-10-26 19:20:47 +0200

    #26 Improve documentation

--- src/main/java/de/b0n/dir/processor/DuplicateContentFinderCallback.java
+++ src/main/java/de/b0n/dir/processor/DuplicateContentFinderCallback.java
@@ -3,12 +3,30 @@ package de.b0n.dir.processor;
 import java.io.File;
 import java.util.List;
 
+/**
+ * Ermöglicht das Tracking des aktuellen Fortschritts auf Dateiebene.
+ * Sobald ein Ergebnis bezüglich einer oder mehrerer Dateien vom DuplicateContentFinder erkannt wurde, wird dieser Callback mit dem oder den Files aufgerufen.
+ * @author Claus
+ *
+ */
 public interface DuplicateContentFinderCallback {
 
+	/**
+	 * Wird aufgerufen, sobald ein Fehler beim Lesen der Datei erkannt wurde
+	 * @param failedFile File mit Zugriffsfehler
+	 */
 	default void failedFile(File failedFile) {}
 
+	/**
+	 * Wird aufgerufen, sobald eine einzigartige Datei erkannt wurde
+	 * @param uniqueFile File ohne Duplikat
+	 */
 	default void uniqueFile(File uniqueFile) {}
 
+	/**
+	 * Wird aufgerufen, sobald eine Gruppe gleicher Dateien erkannt wurde
+	 * @param duplicateFiles Files mit demselben Inhalt
+	 */
 	default void duplicateGroup(List<File> duplicateFiles) {}
 
 }
--- src/main/java/de/b0n/dir/processor/Executor.java
+++ src/main/java/de/b0n/dir/processor/Executor.java
@@ -7,20 +7,31 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+/**
+ * Diese Klasse dient zum vereinfachten Starten von Runnables, welche vor der Weiterverarbeitung abgeschlossen sein sollen.
+ * @author Claus
+ *
+ */
 class Executor {
 	private final ExecutorService THREAD_POOL = Executors.newWorkStealingPool();
 	private final Queue<Future<?>> futures = new ConcurrentLinkedQueue<>();
 
+	/**
+	 * Fügt ein Runnable zur Ausführungsqueue hinzu
+	 * @param runnable zu Startende Nebenläufige Klasse
+	 */
 	public void submit(Runnable runnable) {
 		futures.add(THREAD_POOL.submit(runnable));
 	}
 
+	/**
+	 * Schließt alle Threads im Ausführungsqueue ab, bevor diese Methode zurückkehrt
+	 */
 	public void consolidate() {
 		while (!futures.isEmpty()) {
 			try {
 				futures.remove().get();
 			} catch (InterruptedException e) {
-				// May never occur, Thread is already stopped
 				throw new IllegalStateException("Thread was already stopped", e);
 			} catch (ExecutionException e) {
 				throw new IllegalStateException("Thread could not be consolidated", e);
